400abc85950f4983ba9b24e0d0b8020edc624f36
Agatino Pesce <agatino.pesce@gmail.com>
2023-04-17 20:44:33 +0200

    Add project files.

---
+++ DeconstructTest/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DeconstructTest;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Person p1 = new("a", "p");
+        (string a, string b) = p1;
+        Console.WriteLine($"1) {a} 2) {b}");
+    }
+}
+
+public class Person
+{
+    public Person(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public string FirstName { get; set; }= string.Empty;
+    public string LastName { get; set; }=string.Empty;
+
+    public void Deconstruct(out string f, out string l) 
+    {
+        f=FirstName.ToUpper(); 
+        l=LastName.ToUpper();
+    }
+}
